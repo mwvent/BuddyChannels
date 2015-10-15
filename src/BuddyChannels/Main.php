15f95854b564dab55018da3314a9a474c339d3ca
BuildTools <mattwattz@gmail.com>
2015-10-16 00:04:03 +0100

    Added basic swearing wordlist filter for high false positive swear filtering, swearing punishment for high positive swear detection (turns users text into baby babble, spam filter, added config options for specifying server id for linking chat accross servers in next version

--- src/BuddyChannels/Main.php
+++ src/BuddyChannels/Main.php
@@ -17,70 +17,237 @@ class Main extends PluginBase {
 	const VERSION = "1.0";
 	const MAIN_WEBSITE = "https://wattz.org.uk/mcpe";
 	const PREFIX = "&b[&aBuddy&eChannels&b] ";
+	const BABY_TIMEOUT_SECONDS = 60;
 	
 	public $cfg;
 	public $users;
 	private $users_hasmutedpublic;
+	private $users_babyendtime;
 	public $channelnames;
 	private $db;
 	private $tables;
 	public $website;
+	public $lastmessages; // array of arrays key is player name sub array is recent messages with timestamp
+	private $badWordList;
 	
 	
-	public function translateColors($symbol, $message){
-		
-		$message = str_replace($symbol."0", TextFormat::BLACK, $message);
-		$message = str_replace($symbol."1", TextFormat::DARK_BLUE, $message);
-		$message = str_replace($symbol."2", TextFormat::DARK_GREEN, $message);
-		$message = str_replace($symbol."3", TextFormat::DARK_AQUA, $message);
-		$message = str_replace($symbol."4", TextFormat::DARK_RED, $message);
-		$message = str_replace($symbol."5", TextFormat::DARK_PURPLE, $message);
-		$message = str_replace($symbol."6", TextFormat::GOLD, $message);
-		$message = str_replace($symbol."7", TextFormat::GRAY, $message);
-		$message = str_replace($symbol."8", TextFormat::DARK_GRAY, $message);
-		$message = str_replace($symbol."9", TextFormat::BLUE, $message);
-		$message = str_replace($symbol."a", TextFormat::GREEN, $message);
-		$message = str_replace($symbol."b", TextFormat::AQUA, $message);
-		$message = str_replace($symbol."c", TextFormat::RED, $message);
-		$message = str_replace($symbol."d", TextFormat::LIGHT_PURPLE, $message);
-		$message = str_replace($symbol."e", TextFormat::YELLOW, $message);
-		$message = str_replace($symbol."f", TextFormat::WHITE, $message);
-		
-		$message = str_replace($symbol."k", TextFormat::OBFUSCATED, $message);
-		$message = str_replace($symbol."l", TextFormat::BOLD, $message);
-		$message = str_replace($symbol."m", TextFormat::STRIKETHROUGH, $message);
-		$message = str_replace($symbol."n", TextFormat::UNDERLINE, $message);
-		$message = str_replace($symbol."o", TextFormat::ITALIC, $message);
-		$message = str_replace($symbol."r", TextFormat::RESET, $message);
-		
-		return $message;
+    public function translateColors($symbol, $message) {
+	    $message = str_replace($symbol."0", TextFormat::BLACK, $message);
+	    $message = str_replace($symbol."1", TextFormat::DARK_BLUE, $message);
+	    $message = str_replace($symbol."2", TextFormat::DARK_GREEN, $message);
+	    $message = str_replace($symbol."3", TextFormat::DARK_AQUA, $message);
+	    $message = str_replace($symbol."4", TextFormat::DARK_RED, $message);
+	    $message = str_replace($symbol."5", TextFormat::DARK_PURPLE, $message);
+	    $message = str_replace($symbol."6", TextFormat::GOLD, $message);
+	    $message = str_replace($symbol."7", TextFormat::GRAY, $message);
+	    $message = str_replace($symbol."8", TextFormat::DARK_GRAY, $message);
+	    $message = str_replace($symbol."9", TextFormat::BLUE, $message);
+	    $message = str_replace($symbol."a", TextFormat::GREEN, $message);
+	    $message = str_replace($symbol."b", TextFormat::AQUA, $message);
+	    $message = str_replace($symbol."c", TextFormat::RED, $message);
+	    $message = str_replace($symbol."d", TextFormat::LIGHT_PURPLE, $message);
+	    $message = str_replace($symbol."e", TextFormat::YELLOW, $message);
+	    $message = str_replace($symbol."f", TextFormat::WHITE, $message);
+	    $message = str_replace($symbol."k", TextFormat::OBFUSCATED, $message);
+	    $message = str_replace($symbol."l", TextFormat::BOLD, $message);
+	    $message = str_replace($symbol."m", TextFormat::STRIKETHROUGH, $message);
+	    $message = str_replace($symbol."n", TextFormat::UNDERLINE, $message);
+	    $message = str_replace($symbol."o", TextFormat::ITALIC, $message);
+	    $message = str_replace($symbol."r", TextFormat::RESET, $message);
+	    // $message = str_replace(" ", "ᱹ", $message);
+	    return $message;
+    }
+    
+    public function wordlist_censor($msg) {
+	if( ! $this->read_cfg("use-wordlist-censor", false) ) {
+	    return $msg;
 	}
+	return str_ireplace($this->badWordList,"*",$msg);
+	print_r($this->badWordList);
+    }
+    
+    public function spamTest($player, $msg) {
+	if( ! $this->read_cfg("use-spamfilter", false) ) {
+	    return false;
+	}
+	$origmsg = $msg;
+	$msg = strtolower(str_replace(" ", "", $msg)); // remove spaces and caps
+	$playername = $player->getName();
+	// if no history store msg and return
+	if( ! isset($this->lastmessages[$playername]) ) {
+	    $playername = strtolower($playername);
+	    $this->lastmessages[$playername] = array();
+	    $this->lastmessages[$playername][time()] = $msg;
+	    return false;
+	}
+	// save message in array
+	$thismsgtime = time();
+	$this->lastmessages[$playername][$thismsgtime] = strtolower($msg);
+	// has history - clear any items older than spamcheck_message_age config value
+	foreach(  $this->lastmessages[$playername] as $msgtime => $prevmsg  ) {
+	    if( ( time() - $msgtime ) > $this->read_cfg("spamfilter-time-interval", 60) ) {
+		unset( $this->lastmessages[$playername][$msgtime] );
+	    }
+	}
+	// now return true if player has posted more than two of the same in the remaining interval
+	$counted_prevMsgs = array_count_values( $this->lastmessages[$playername] );
+	if( isset( $counted_prevMsgs[$msg] ) ) {
+	    if( $counted_prevMsgs[$msg] > $this->read_cfg("spamfilter-max-duplicates-per-interval", 2) ) {
+		$sendmsg = "&cYour message has failed spam detection and has been blocked.";
+		$player->sendMessage($this->translateColors("&", $sendmsg));
+		$sendmsg = $playername . "'s message was block by spam filter - message was - " . $origmsg;
+		Server::getInstance()->getLogger()->info($this->translateColors("&" , Main::PREFIX . $sendmsg));
+		unset( $this->lastmessages[$playername][$thismsgtime] );
+		return true;
+	    }
+	}
+	
+	// now return true if rate limit exceeded ( msgs in the  history interval )
+	$count_of_recent = count( $this->lastmessages[$playername] );
+	if ( $count_of_recent >= $this->read_cfg("spamfilter-max-messages-per-interval", 6) ) {
+	    $sendmsg = "&cYou have exceeded the chat rate limit of "
+		       . $this->read_cfg("spamfilter-max-messages-per-interval", 6)
+		       . " messages per " . $this->read_cfg("spamfilter-time-interval", 60) . "s";
+	    $player->sendMessage($this->translateColors("&", $sendmsg));
+	    $sendmsg = $playername . "'s message was block by rate filter - message was - " . $origmsg;
+	    Server::getInstance()->getLogger()->info($this->translateColors("&" , Main::PREFIX . $sendmsg));
+	    unset( $this->lastmessages[$playername][$thismsgtime] );
+	    return true;
+	}
+	
+	// otherwise all is ok
+	return false;
+    }
+    
+    public function hasVeryBadLanguage($msg) {
+	if( ! $this->read_cfg("swearing-is-immature", false) ) {
+	    return false;
+	}
+	$reallybadwords = array(
+	    "fuck",
+	    "fucking",
+	    "fuking",
+	    "bitch",
+	    "bastard",
+	    "fucker"
+	);
+	$permittedVariations = array(
+	    "itch",
+	    "uck"
+	);
+	
+	$colourCodes = str_split("1234567890abcdefghijklmnopqrstuvwxyz");
+	foreach($colourCodes as $key => $code) {
+	    $colourCodes[$key] = "&" . $code;
+	}
+
+	// make 1 letter missing variations
+	$badwordlist = array();
+	foreach($reallybadwords as $badword) {
+	    $badwordlist[] = $badword;
+	    $letters = str_split($badword);
+	    for($i = 0; $i<count($letters); $i++) {
+		$variation = "";
+		for($x = 0; $x<count($letters); $x++) {
+		    if($x <> $i) $variation .= $letters[$x];
+		}
+		if( ! in_array($variation, $permittedVariations) ) $badwordlist[] = $variation;
+	    }
+	}
+	
+	// manual (non fudged) additions
+	// $badwordlist[] = "shit";
+	$badwordlist[] = "cunt";
+	$badwordlist[] = "nigger";
+	$badwordlist[] = "nigga";
+	$badwordlist[] = "f***";
+	$badwordlist[] = "s***";
+	
+	
+	// replace caps
+	$msg = strtolower($msg);
+	// replace l33t chars
+	$msg = str_replace("1", "i", $msg);
+	$msg = str_replace("3", "e", $msg);
+	$msg = str_replace("4", "a", $msg);
+	$msg = str_replace("5", "s", $msg);
+	$msg = str_replace("7", "t", $msg);
+	$msg = str_replace("8", "ate", $msg);
+	// replace codes
+	$msg = str_replace($colourCodes, "", $msg);
+	// replace some pronouns
+	$msg = str_replace("you", "", $msg);
+	// take out non alphanumeric chars
+	$msg = preg_replace('/[^a-z\d ]/i', '', $msg);	
+	echo $msg . PHP_EOL;
+	$msg_words = explode(" ", $msg);
+	// add an entry with the spaces taken out
+	$msg_words[] =  str_replace(" ", "", $msg);
+	// add entry with i's replaced by 1s
+	$msg_words[] =  str_replace( "i", "1", str_replace(" ", "", $msg));
+	
+	foreach($badwordlist as $badword) {
+		$badword = strtolower($badword);
+		if(in_array($badword, $msg_words)) {
+		    return true;
+		}
+		foreach($msg_words as $msg_word) {
+		    if( ! stripos($msg_word, $badword) === false ) {
+			return true;
+		    }
+		}
+	}
+    }
+    
+    public function read_cfg($key, $defaultvalue = null) {
+	// if not loaded config load and continue
+	if( ! isset($this->cfg) ) {
+	    $this->cfg = $this->getConfig()->getAll();
+	}
+	// if key not in config but a default value is allowed return default
+	if( ( ! isset($this->cfg[$key]) ) && ( ! is_null( $defaultvalue ) ) ) {
+	    return $defaultvalue;
+	}
+	// if key not in config but is required
+	if( ( ! isset($this->cfg[$key]) ) && ( ! is_null( $defaultvalue ) ) ) {
+	    $sendmsg = "Cannot load " . Main::PREFIX . " required config key " . $key . " not found in config file";
+	    Server::getInstance()->getLogger()->critical($this->translateColors("&", Main::PREFIX . $sendmsg));
+	    die();
+	}
+	// otherwise return config file value
+	return $this->cfg[$key];
+    }
 	
     public function onEnable(){
         @mkdir($this->getDataFolder());
         $this->saveDefaultConfig();
-        $this->cfg = $this->getConfig()->getAll();
         $this->getCommand("buddychannels")->setExecutor(new Commands\Commands($this));
         $this->getCommand("shout")->setExecutor(new Commands\Commands($this));
 	$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
 	$this->channelnames = array("0" => "Public");
 	$this->users_hasmutedpublic = array();
-	// TODO default values if not in array
+	$this->users_babyendtime = array();
+	$this->lastmessages = array();
+	if( $this->read_cfg("use-wordlist-censor", false) ) {
+	    $this->badWordList = file($this->getDataFolder() . "/badwords.txt", FILE_IGNORE_NEW_LINES);
+	}
+	    
 	$this->tables = array(
-	    "bpgroupmembers" => $this->cfg["buddypress-bp_groups_members-tablename"],
-	    "bpgroups" => $this->cfg["buddypress-bp_groups-tablename"],
-	    "wpusers" => $this->cfg["wordpress-users-tablename"],
-	    "bcusers" => $this->cfg["buddychannels-users-table"],
-	    "chatlog" => $this->cfg["buddychannels-chatlog-table"]
+	    "bpgroupmembers" => $this->read_cfg("buddypress-bp_groups_members-tablename"),
+	    "bpgroups" => $this->read_cfg("buddypress-bp_groups-tablename"),
+	    "wpusers" => $this->read_cfg("wordpress-users-tablename"),
+	    "bcusers" => $this->read_cfg("buddychannels-users-table"),
+	    "chatlog" => $this->read_cfg("buddychannels-chatlog-table"),
+	    "servers" => $this->read_cfg("buddychannels-servers-table")
 	);
 	
-	$this->website = $this->cfg["website"];
+	$this->website = $this->read_cfg("website");
 	
 	$this->db = new \mysqli(
-	    $this->cfg["mysql-server"],
-	    $this->cfg["mysql-user"], 
-	    $this->cfg["mysql-pass"], 
-	    $this->cfg["database"]
+	    $this->read_cfg("mysql-server"),
+	    $this->read_cfg("mysql-user"), 
+	    $this->read_cfg("mysql-pass"), 
+	    $this->read_cfg("database")
 	);
 	if ($this->db->connect_errno) {
 	    trigger_error("DB Error");
@@ -101,17 +268,49 @@ class Main extends PluginBase {
 	$sql = "
 	    CREATE TABLE IF NOT EXISTS `" . $this->tables["chatlog"] . "` (
 		`messagetime` DATETIME DEFAULT CURRENT_TIMESTAMP,
-		`serverid` INT, 
+		`serverid` INT,
 		`username` VARCHAR(50),
 		`channel` INT,
 		`channelname` TEXT,
-		`message` TEXT
+		`message` TEXT,
+		`unfiltered_message` TEXT,
+		`was_baby` TINYINT,
+		`was_shouting` TINYINT
 	    )
 	    ;
 	";
 	if( ! $this->db->query($sql) ) {
 	    Server::getInstance()->getLogger()->critical(
 		$this->translateColors("&", Main::PREFIX  . "DB Error " . $this->db->error));
+	}
+	$sql = "
+	    CREATE TABLE IF NOT EXISTS `" . $this->tables["servers"] . "` (
+		`serverid` INT,
+		`servername` VARCHAR(50),
+		`connect_chat` TINYINT,
+		PRIMARY KEY (`serverid`)
+	    )
+	    ;
+	";
+	if( ! $this->db->query($sql) ) {
+	    Server::getInstance()->getLogger()->critical(
+		$this->translateColors("&", Main::PREFIX  . "DB Error " . $this->db->error));
+	}
+	$sql = "
+	    INSERT INTO `" . $this->tables["servers"] . "` 
+		(`serverid`, `servername`, `connect_chat`)
+	    VALUES
+		( '" . $this->read_cfg("server-id", 0) .  "',
+		'" . $this->read_cfg("server-name", "untitled") . "' ,
+		'" . $this->read_cfg("connect-server-chat", false) . "')
+	    ON DUPLICATE KEY UPDATE 
+		`serverid` = '" . $this->read_cfg("server-id", 0) . "',
+		`servername` = '" . $this->read_cfg("server-name", "untitled") . "',
+		`connect_chat` = '" . $this->read_cfg("connect-server-chat", false) . "'
+	    ;
+	";
+	if( ! $this->db->query($sql) ) {
+	    trigger_error("DB Error" . $this->db->error);
 	}
 	Server::getInstance()->getLogger()->info(
 	    $this->translateColors("&", Main::PREFIX  . "Ready"));
@@ -301,11 +500,73 @@ class Main extends PluginBase {
 	}
 	$message_elements[] = $this->getPlayerRank($player);
 	$message_elements[] = $player->getName();
-	$message_elements[] = "&a> &r" . $message;
+	$message_elements[] = "&a‣&r" . $message;
 	return implode("&r&f ", $message_elements);
     }
     
+    public function babyFilter(Player $player, $message) {
+	// check for extreme bad language (words that have higher occurance of false positive can be handled
+	// by a general swear filter
+	$is_baby = false;
+	$msg_has_profanity = $this->hasVeryBadLanguage($message);
+	// already on a swearing punishment?
+	if( isset($this->users_babyendtime[$player->getName()]) ) {
+	    $bendtime = $this->users_babyendtime[$player->getName()];
+	    $timeleft = ( $bendtime - time() );
+	    if( $timeleft > 0 ) {
+		$is_baby = true;
+		// now we will tell the user they are still a baby - unless they have swore again, that will be handled below
+		if( ! $msg_has_profanity ) {
+		    $sendmsg = $this->translateColors("&" , "&3You are still a baby for the next &c" . $timeleft . "&3 seconds.");
+		    $player->sendMessage($sendmsg);
+		}
+	    } else {
+		$is_baby = false;
+		unset($this->users_babyendtime[$player->getName()]);
+		$sendmsg = $this->translateColors("&" , "&3You are no longer a baby - may we please remind you that swearing can result in a permanent ban.");
+		$player->sendMessage($sendmsg);
+	    }
+	}
+	// swore (again?)
+	if($msg_has_profanity) {
+	    $sendmsg = $this->translateColors("&" , "&cWe have detected you are using profanity.");
+	    $player->sendMessage($sendmsg);
+	    $sendmsg = Main::PREFIX . $player->getName() . " activated baby swear filter with message " . $message;
+	    Server::getInstance()->getLogger()->info($this->translateColors("&" , $sendmsg));	
+	    // swore while not already a baby
+	    if( ! $is_baby ) {
+		$sendmsg = "&3Swearing is for babies - You are now a baby for &c" . $this->read_cfg("swearing-is-immature-punishment-period", 60) . "&3 seconds.";
+		$player->sendMessage($this->translateColors("&" , $sendmsg));
+		$is_baby = true;
+		$this->users_babyendtime[$player->getName()] = ( time() + $this->read_cfg("swearing-is-immature-punishment-period", 60) );
+	    } else { // swore while already a baby
+		$bendtime = $this->users_babyendtime[$player->getName()];
+		$timeleft = (( $bendtime - time() ) * 2 ) + $this->read_cfg("swearing-is-immature-punishment-period", 60);
+		$this->users_babyendtime[$player->getName()] = ( time() + $timeleft );
+		$sendmsg = "&3Your time as a baby has been extended to &c" . $timeleft . "&3 seconds.";
+		$player->sendMessage($this->translateColors("&" , $sendmsg));
+		$is_baby = true;
+	    }
+	}
+	
+	// if a baby - pre-format the message
+	if( $is_baby ) {
+	    $babyisms = array("goo","ga", "gurgle");
+	    $msg_words = explode(" ", $message);
+	    $message = "";
+	    foreach($msg_words as $i) { $message .= $babyisms[array_rand($babyisms)] . " "; }    
+	}
+	return $message;
+    }
+    
     public function SendChannelMessage(Player $player, $message, $shouting = false) {
+	$unfiltered_message = $message;
+    
+	// check for spam
+	if( $this->spamTest($player, $message) ) {
+	    return;
+	}
+	
         // (re) init users channel
         $channelnumber = $this->getPlayersChannel($player);
         $channelname = $this->channelnames[$channelnumber];
@@ -314,7 +575,14 @@ class Main extends PluginBase {
 	// player can always receive their own echo
 	$names_of_players_receiving_message = array(strtolower($player->getName()));
 	
-	// format the message	
+	// format the message
+	$is_baby = false;
+	$babyfiltered = $this->babyFilter($player, $message);
+	if($babyfiltered != $message) {
+	    $is_baby = true;
+	    $message = $babyfiltered;
+	}
+	$message = $this->wordlist_censor($message);
 	$message_formatted = $this->translateColors("&", $this->getChannelFormat($channelnumber, $channelname, $player, $message));
 	$message_formatted_sc = $this->translateColors("&", $this->getChannelFormat($channelnumber, $channelname, $player, $message, true));
 	
@@ -374,13 +642,16 @@ class Main extends PluginBase {
 	}
 	//TODO Log chat to db
 	$sqlvars = array();
-	$sqlvars[] = "'1'";
+	$sqlvars[] = "'" . $this->db->real_escape_string( $this->read_cfg("server-id", 0) ) . "'";
 	$sqlvars[] = "'" . $this->db->real_escape_string( $player->getName() ) . "'";
 	$sqlvars[] = "'" . $this->db->real_escape_string( $channelnumber ) . "'";
 	$sqlvars[] = "'" . $this->db->real_escape_string( $channelname ) . "'";
 	$sqlvars[] = "'" . $this->db->real_escape_string( $message ) . "'";
+	$sqlvars[] = "'" . $this->db->real_escape_string( $unfiltered_message ) . "'";
+	$sqlvars[] = "'" . $this->db->real_escape_string( $is_baby ) . "'";
+	$sqlvars[] = "'" . $this->db->real_escape_string( $shouting ) . "'";
 	$sql = "INSERT INTO `" . $this->tables["chatlog"] . "`
-		(`serverid`, `username`, `channel`, `channelname`, `message`)
+		(`serverid`, `username`, `channel`, `channelname`, `message`, `unfiltered_message`, `was_baby`, `was_shouting`)
 		VALUES ( " . implode(",", $sqlvars) . " )
 		;
 	";
